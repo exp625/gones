e561e7afe6b31b74ad84136d466e381e01ce40d4
Tim <xmrtimex@gmail.com>
2021-11-16 15:11:38 +0100

    :construction: Add PPU scanline and position to check timing

--- nes/ppu.go
+++ nes/ppu.go
@@ -1,14 +1,35 @@
 package nes
 
 type PPU struct {
+	ScanLine uint16
+	Position uint16
+	FrameCount uint64
 }
 
 func (ppu *PPU) Clock() {
 
+	if ppu.ScanLine < 261 {
+		if ppu.Position < 340 {
+			ppu.Position++
+		} else {
+			ppu.Position = 0
+			ppu.ScanLine++
+		}
+	} else {
+		ppu.Position = 0
+		ppu.ScanLine = 0
+		ppu.FrameCount++
+		if ppu.FrameCount % 2 != 0 {
+			ppu.Position++
+		}
+	}
+
 }
 
 func (ppu *PPU) Reset() {
-
+	ppu.ScanLine = 0
+	ppu.Position = 0
+	ppu.FrameCount = 0
 }
 
 func (ppu *PPU) Read(location uint16) (bool, uint8) {
