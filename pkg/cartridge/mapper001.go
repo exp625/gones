18e4fa5128763e22b845d82168c9352d70adbc5f
Tim <xmrtimex@gmail.com>
2022-01-08 23:36:20 +0100

    :art: Try to implement all MMC1 features and fail

--- pkg/cartridge/mapper001.go
+++ pkg/cartridge/mapper001.go
@@ -8,20 +8,23 @@ import (
 )
 
 type Mapper001 struct {
-	cartridge     *Cartridge
-	shiftRegister shift_register.ShiftRegister8
-	prgRam        [0x2000]uint8
-	control       uint8
-	chrBanks      [2]uint8
-	prgBanks      [1]uint8
+	cartridge      *Cartridge
+	shiftRegister  shift_register.ShiftRegister8
+	prgRam         [0x8000]uint8
+	control        uint8
+	chrBanks       [2]uint8
+	prgBanks       [1]uint8
+	prgBanksDouble uint8
+	ramBanks       [1]uint8
+	ramEnable      bool
 }
 
 func NewMapper001(c *Cartridge) *Mapper001 {
 	m := &Mapper001{
 		cartridge: c,
 	}
-	// We use the initial 1 to check when the register was shifted 5 times
-	m.shiftRegister.Set(1)
+	// We use the initial 0b1000_0000 to check when the register was shifted 5 times
+	m.shiftRegister.Set(0b1000_0000)
 	m.control = 0x0C
 	return m
 }
@@ -36,7 +39,7 @@ func (m *Mapper001) CPUMap(location uint16) uint16 {
 func (m *Mapper001) CPURead(location uint16) uint8 {
 	if 0x6000 <= location && location <= 0x7FFF {
 		// CPU $6000-$7FFF: 8 KB PRG RAM bank, (optional)
-		return m.prgRam[location-0x6000]
+		return m.prgRam[uint64(location-0x6000)+0x2000*uint64(m.ramBanks[0])]
 	}
 
 	if 0x8000 <= location && location <= 0xBFFF {
@@ -44,19 +47,13 @@ func (m *Mapper001) CPURead(location uint16) uint8 {
 		switch (m.control >> 2) & 0b11 {
 		case 2:
 			// 2: fix first bank at $8000 and switch 16 KB bank at $C000
-			return m.cartridge.PrgRom[uint64(location-0x8000)]
+			return m.cartridge.PrgRom[uint64(location-0x8000)|(uint64(m.prgBanksDouble)<<18)]
 		case 3:
 			// 3: fix last bank at $C000 and switch 16 KB bank at $8000
-			if m.prgBanks[0] > m.cartridge.PrgRomSize {
-				return 0
-			}
-			return m.cartridge.PrgRom[uint64(location-0x8000)+0x4000*uint64(m.prgBanks[0])]
+			return m.cartridge.PrgRom[uint64(location-0x8000)+0x4000*uint64(m.prgBanks[0])|(uint64(m.prgBanksDouble)<<18)]
 		default:
 			// 0, 1: switch 32 KB at $8000, ignoring low bit of bank number
-			if m.prgBanks[0]&0b1110 > m.cartridge.PrgRomSize {
-				return 0
-			}
-			return m.cartridge.PrgRom[uint64(location-0x8000)+0x4000*uint64(m.prgBanks[0]&0b1110)]
+			return m.cartridge.PrgRom[uint64(location-0x8000)+0x4000*uint64(m.prgBanks[0]&0b1110)|(uint64(m.prgBanksDouble)<<18)]
 		}
 	}
 
@@ -65,19 +62,13 @@ func (m *Mapper001) CPURead(location uint16) uint8 {
 		switch (m.control >> 2) & 0b11 {
 		case 2:
 			// 2: fix first bank at $8000 and switch 16 KB bank at $C000
-			if m.prgBanks[0] > m.cartridge.PrgRomSize {
-				return 0
-			}
-			return m.cartridge.PrgRom[uint64(location-0xC000)+0x4000*uint64(m.prgBanks[0])]
+			return m.cartridge.PrgRom[uint64(location-0xC000)+0x4000*uint64(m.prgBanks[0])|(uint64(m.prgBanksDouble)<<18)]
 		case 3:
 			// 3: fix last bank at $C000 and switch 16 KB bank at $8000
-			return m.cartridge.PrgRom[uint64(location-0xC000)+0x4000*uint64(m.cartridge.PrgRomSize-1)]
+			return m.cartridge.PrgRom[uint64(location-0xC000)+0x4000*uint64(m.cartridge.PrgRomSize-1)|(uint64(m.prgBanksDouble)<<18)]
 		default:
-			if m.prgBanks[0]&0b1110 > m.cartridge.PrgRomSize {
-				return 0
-			}
 			// 0, 1: switch 32 KB at $8000, ignoring low bit of bank number
-			return m.cartridge.PrgRom[uint64(location-0xC000)+0x4000*uint64(m.prgBanks[0]&0b1110)+0x4000]
+			return m.cartridge.PrgRom[uint64(location-0xC000)+0x4000*uint64(m.prgBanks[0]&0b1110)+0x4000|(uint64(m.prgBanksDouble)<<18)]
 		}
 	}
 
@@ -87,7 +78,7 @@ func (m *Mapper001) CPURead(location uint16) uint8 {
 func (m *Mapper001) CPUWrite(location uint16, data uint8) bool {
 	if location >= 0x6000 && location <= 0x7FFF {
 		// CPU $6000-$7FFF: 8 KB PRG RAM bank, (optional)
-		m.prgRam[location-0x6000] = data
+		m.prgRam[uint64(location-0x6000)+0x2000*uint64(m.ramBanks[0])] = data
 		return true
 	}
 	if location >= 0x8000 {
@@ -99,14 +90,14 @@ func (m *Mapper001) CPUWrite(location uint16, data uint8) bool {
 		// |       +- Data bit to be shifted into shift register, LSB first
 		// +--------- 1: Reset shift register and write Control with (Control OR $0C),
 		//               locking PRG ROM at $C000-$FFFF to the last bank.
-		m.shiftRegister.ShiftLeft(data & 0b1)
+		m.shiftRegister.ShiftRight(data & 0b1)
 		if data>>7&0b1 == 1 {
 			// Reset shift register
 			// We use the initial 1 to check when the register was shifted 5 times
-			m.shiftRegister.Set(1)
+			m.shiftRegister.Set(0b1000_0000)
 			m.control = 0x0C
 		}
-		if m.shiftRegister.GetBit(5) == 1 {
+		if m.shiftRegister.GetBit(2) == 1 {
 			switch {
 			case 0x8000 <= location && location <= 0x9FFF:
 				// Control (internal, $8000-$9FFF)
@@ -120,7 +111,16 @@ func (m *Mapper001) CPUWrite(location uint16, data uint8) bool {
 				// |                         2: fix first bank at $8000 and switch 16 KB bank at $C000;
 				// |                         3: fix last bank at $C000 and switch 16 KB bank at $8000)
 				// +----- CHR ROM bank mode (0: switch 8 KB at a time; 1: switch two separate 4 KB banks)
-				m.control = m.shiftRegister.Get() & 0b0001_1111
+				m.control = m.shiftRegister.Get() >> 3
+				if m.cartridge.ChrRomSize <= 4 {
+					// TODO: Currently breaks
+					// m.ramBanks[0] = m.shiftRegister.Get() >> 5 & 0b11
+				}
+				if m.cartridge.PrgRomSize == 32 && m.shiftRegister.Get()>>7 == 1 {
+					m.prgBanksDouble = 1
+				} else {
+					m.prgBanksDouble = 0
+				}
 			case 0xA000 <= location && location <= 0xBFFF:
 				// CHR bank 0 (internal, $A000-$BFFF)
 				// 4bit0
@@ -128,7 +128,16 @@ func (m *Mapper001) CPUWrite(location uint16, data uint8) bool {
 				// CCCCC
 				// |||||
 				// +++++- Select 4 KB or 8 KB CHR bank at PPU $0000 (low bit ignored in 8 KB mode)
-				m.chrBanks[0] = m.shiftRegister.Get() & 0b0001_1111
+				m.chrBanks[0] = m.shiftRegister.Get() >> 3 & (m.cartridge.ChrRomSize - 1)
+				if m.cartridge.ChrRomSize <= 4 {
+					// TODO: Currently breaks 
+					m.ramBanks[0] = m.shiftRegister.Get() >> 5 & 0b11
+				}
+				if m.cartridge.PrgRomSize == 32 && m.shiftRegister.Get()>>7 == 1 {
+					m.prgBanksDouble = 1
+				} else {
+					m.prgBanksDouble = 0
+				}
 			case 0xC000 <= location && location <= 0xDFFF:
 				// CHR bank 1 (internal, $C000-$DFFF)
 				// 4bit0
@@ -136,7 +145,7 @@ func (m *Mapper001) CPUWrite(location uint16, data uint8) bool {
 				// CCCCC
 				// |||||
 				// +++++- Select 4 KB CHR bank at PPU $1000 (ignored in 8 KB mode)
-				m.chrBanks[1] = m.shiftRegister.Get() & 0b0001_1111
+				m.chrBanks[1] = m.shiftRegister.Get() >> 3 & (m.cartridge.ChrRomSize - 1)
 			case 0xE000 <= location:
 				// PRG bank (internal, $E000-$FFFF)
 				// 4bit0
@@ -146,11 +155,12 @@ func (m *Mapper001) CPUWrite(location uint16, data uint8) bool {
 				// |++++- Select 16 KB PRG ROM bank (low bit ignored in 32 KB mode)
 				// +----- MMC1B and later: PRG RAM chip enable (0: enabled; 1: disabled; ignored on MMC1A)
 				//        MMC1A: Bit 3 bypasses fixed bank logic in 16K mode (0: affected; 1: bypassed)
-				m.prgBanks[0] = m.shiftRegister.Get() & 0b0000_1111
+				m.prgBanks[0] = m.shiftRegister.Get() >> 3 & 0b0000_1111
+				m.ramEnable = m.shiftRegister.Get()>>7 == 0
 			}
 			// Reset shift register
-			// We use the initial 1 to check when the register was shifted 5 times
-			m.shiftRegister.Set(1)
+			// We use the initial 0b1000_0000 to check when the register was shifted 5 times
+			m.shiftRegister.Set(0b1000_0000)
 		}
 		return true
 	}
@@ -204,19 +214,18 @@ func (m *Mapper001) PPURead(location uint16) uint8 {
 }
 
 func (m *Mapper001) PPUWrite(location uint16, data uint8) bool {
-	if location <= 0x1FFF {
-		if m.cartridge.ChrRam {
-			// CHR RAM
-			if m.control>>4&0b1 == 0 {
-				// 0: switch 8 KB at a time;
-				m.cartridge.ChrRom[uint64(location)+0x1000*uint64(m.chrBanks[0]&0b1110)] = data
+
+	if m.cartridge.ChrRam {
+		// CHR RAM
+		if m.control>>4&0b1 == 0 {
+			// 0: switch 8 KB at a time;
+			m.cartridge.ChrRom[uint64(location)+0x1000*uint64(m.chrBanks[0]&0b1110)] = data
+		} else {
+			//  1: switch two separate 4 KB banks
+			if location <= 0x0FFF {
+				m.cartridge.ChrRom[uint64(location)+0x1000*uint64(m.chrBanks[0])] = data
 			} else {
-				//  1: switch two separate 4 KB banks
-				if location <= 0x0FFF {
-					m.cartridge.ChrRom[uint64(location)+0x1000*uint64(m.chrBanks[0])] = data
-				} else {
-					m.cartridge.ChrRom[(uint64(location)-0x1000)+0x1000*uint64(m.chrBanks[1])] = data
-				}
+				m.cartridge.ChrRom[(uint64(location)-0x1000)+0x1000*uint64(m.chrBanks[1])] = data
 			}
 		}
 		return true
@@ -225,11 +234,13 @@ func (m *Mapper001) PPUWrite(location uint16, data uint8) bool {
 }
 
 func (m *Mapper001) Reset() {
-	// We use the initial 1 to check when the register was shifted 5 times
-	m.shiftRegister.Set(1)
+	// We use the initial 0b1000_0000 to check when the register was shifted 5 times
+	m.shiftRegister.Set(0b1000_0000)
 	m.control = 0x0C
 	m.chrBanks = [2]uint8{}
 	m.prgBanks = [1]uint8{}
+	m.ramBanks = [1]uint8{}
+	m.prgBanksDouble = 0
 }
 
 func (m *Mapper001) CPUClock() {
